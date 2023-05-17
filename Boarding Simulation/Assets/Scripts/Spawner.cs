c1cfb8e2a253688f101f126628ba7faf12fe579d
Oscar Olsson <oscols@kth.se>
2023-05-17 15:00:14 +0200

    steffen and btf list

--- Boarding Simulation/Assets/Scripts/Spawner.cs
+++ Boarding Simulation/Assets/Scripts/Spawner.cs
@@ -51,6 +51,18 @@ public class Spawner : MonoBehaviour {
 	"Seat48", "Seat49", "Seat50", "Seat51", "Seat52", "Seat53", "Seat54", "Seat55", "Seat56", "Seat57", "Seat58", "Seat59", "Seat60", "Seat61",
 	"Seat62", "Seat63", "Seat64", "Seat65", "Seat66", "Seat67", "Seat68", "Seat69", "Seat70", "Seat71", "Seat72"};
 
+	internal List<string> steffen = new List<string>() {"Seat67", "Seat55", "Seat43", "Seat31", "Seat19", "Seat7", "Seat72", "Seat60", "Seat48", "Seat36", "Seat24", "Seat12", "Seat61",
+	"Seat49", "Seat37", "Seat25", "Seat13", "Seat1", "Seat66", "Seat54", "Seat42", "Seat30", "Seat18", "Seat6", "Seat68", "Seat56", "Seat44", "Seat32", "Seat20", "Seat8",
+	"Seat71", "Seat59", "Seat57", "Seat35", "Seat23", "Seat11", "Seat62", "Seat50", "Seat38", "Seat26", "Seat14", "Seat2", "Seat65", "Seat53", "Seat41", "Seat29", "Seat17", 
+	"Seat5", "Seat69", "Seat57", "Seat45", "Seat33", "Seat21", "Seat9", "Seat70", "Seat58", "Seat46", "Seat34", "Seat22", "Seat10", "Seat63",
+	"Seat51", "Seat39", "Seat27", "Seat15", "Seat3", "Seat64", "Seat52", "Seat40", "Seat28", "Seat16", "Seat4"};
+
+	internal List<string> backToFront = new List<string>() {"Seat67", "Seat72", "Seat68", "Seat71", "Seat69", "Seat70", "Seat61", "Seat66", "Seat62", "Seat65", "Seat63", "Seat64", "Seat55",
+	"Seat60", "Seat56", "Seat59", "Seat57", "Seat58", "Seat49", "Seat54", "Seat50", "Seat53", "Seat51", "Seat52", "Seat43", "Seat48", "Seat44", "Seat47", "Seat45", "Seat46",
+	"Seat37", "Seat42", "Seat38", "Seat41", "Seat39", "Seat40", "Seat31", "Seat36", "Seat32", "Seat35", "Seat33", "Seat34", "Seat25", "Seat30", "Seat26", "Seat29", "Seat27", 
+	"Seat28", "Seat19", "Seat24", "Seat20", "Seat23", "Seat21", "Seat22", "Seat13", "Seat18", "Seat14", "Seat17", "Seat15", "Seat16", "Seat7",
+	"Seat12", "Seat8", "Seat11", "Seat9", "Seat10", "Seat1", "Seat6", "Seat2", "Seat5", "Seat3", "Seat4"};
+
 	internal List<string> wilmaOne = new List<string>() {"Seat1", "Seat6", "Seat7", "Seat12", "Seat13", "Seat18", "Seat19", "Seat24", "Seat25", "Seat30", "Seat31", "Seat36", "Seat37",
 	"Seat42", "Seat43", "Seat48", "Seat49", "Seat54", "Seat55", "Seat60", "Seat61", "Seat66", "Seat67", "Seat72"};
 
